2b476c695c18ceff87c65a6a734bad1c640ffdc0
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2025-06-17 14:29:09 +0300

    Custom currencies, transfers. Work in progress.

--- collector/transfers.py
+++ collector/transfers.py
@@ -1,31 +1,40 @@
 from typing import Any
 
-from multiversx_sdk import Address
+from multiversx_sdk import Address, Token, TokenTransfer
+from multiversx_sdk.core.constants import \
+    EGLD_IDENTIFIER_FOR_MULTI_ESDTNFT_TRANSFER
 
-from collector.utils import format_amount
+from collector.utils import ICurrencyProvider, format_amount
 
 
-class Transfer:
-    def __init__(self, sender: Address, label: str, amount: int, token_identifier: str = "") -> None:
+class MyTransfer:
+    def __init__(self, sender: Address, label: str, token_transfer: TokenTransfer) -> None:
         self.sender = sender
         self.label = label
-        self.amount = amount
-        self.token_identifier = token_identifier
+        self.token_transfer = token_transfer
 
     @classmethod
     def new_from_dictionary(cls, data: dict[str, Any]):
         sender = Address.new_from_bech32(data["sender"])
         label = data["label"]
         amount = int(data["amount"])
-        token_identifier = data.get("tokenIdentifier", "")
+        token_identifier = data.get("tokenIdentifier", EGLD_IDENTIFIER_FOR_MULTI_ESDTNFT_TRANSFER)
+        token_nonce = data.get("tokenNonce", 0)
 
-        return cls(sender, label, amount, token_identifier)
+        token = Token(token_identifier, token_nonce)
+        token_transfer = TokenTransfer(token, amount)
+        return cls(sender, label, token_transfer)
 
-    def to_dictionary(self) -> dict[str, Any]:
+    def to_dictionary(self, currency_provider: ICurrencyProvider) -> dict[str, Any]:
+        amount = self.token_transfer.amount
+        token_identifier = self.token_transfer.token.identifier
+        token_nonce = self.token_transfer.token.nonce
+
         return {
             "sender": self.sender.to_bech32(),
             "label": self.label,
-            "amount": self.amount,
-            "amountFormatted": f"{format_amount(self.amount, num_decimals=6)} {self.token_identifier}",
-            "tokenIdentifier": self.token_identifier
+            "amount": amount,
+            "amountFormatted": f"{format_amount(currency_provider, amount, token_identifier)}",
+            "tokenIdentifier": token_identifier,
+            "tokenNonce": token_nonce
         }
